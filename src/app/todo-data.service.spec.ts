3be0f6001587f5008b26228bef9a8f30ed5af22a
Nikita Kasatkin <nikita.kasatkin@exactprosystems.com>
2017-10-22 16:21:57 +0300

    part 3

---
+++ src/app/todo-data.service.spec.ts
@@ -0,0 +1,21 @@
+import {TestBed, inject} from '@angular/core/testing';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+import { ApiService } from './api.service';
+import { ApiMockService} from "./api-mock.service";
+
+describe('TodoDataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoDataService, {
+        provide: ApiService,
+        useClass: ApiMockService
+      }]
+    });
+  });
+
+  it('should ...', inject([TodoDataService], (service: TodoDataService) => {
+    expect(service).toBeTruthy();
+  }));
+
+});
